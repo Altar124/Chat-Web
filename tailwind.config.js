159c54edb367337c079cebe27472172c6f26c06e
Altar124 <hanzzgilang777@gmail.com>
2025-10-27 17:48:53 +0700

    ready for vercel deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
-  theme: { extend: {} },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
